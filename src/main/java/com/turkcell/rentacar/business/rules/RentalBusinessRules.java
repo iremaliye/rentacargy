778cc0a3ff5f20e6fbd3b563eaafc841f1a5b458
İrem Aliye Akman <78559879+iremaliye@users.noreply.github.com>
2024-03-26 15:08:18 +0300

    Projenin temelleri oluşturuldu

---
+++ src/main/java/com/turkcell/rentacar/business/rules/RentalBusinessRules.java
@@ -0,0 +1,40 @@
+package com.turkcell.rentacar.business.rules;
+
+import com.turkcell.rentacar.business.messages.MaintenanceMessages;
+import com.turkcell.rentacar.business.messages.RentalMessages;
+import com.turkcell.rentacar.core.utilities.exceptions.types.BusinessException;
+import com.turkcell.rentacar.dataAccess.abstracts.CarRepository;
+import com.turkcell.rentacar.dataAccess.abstracts.RentalRepository;
+import com.turkcell.rentacar.entities.concretes.Car;
+import com.turkcell.rentacar.entities.concretes.Maintenance;
+import com.turkcell.rentacar.entities.concretes.Rental;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class RentalBusinessRules {
+    private RentalRepository rentalRepository;
+    private CarRepository carRepository;
+
+     public void idIsNotExists (int id){
+        Optional<Rental> rental=this.rentalRepository.findById(id);
+        if(rental.isEmpty()){
+            throw new BusinessException(RentalMessages.idIsnotExists);
+        }
+    }
+    public void currentlyRented (int id){
+        Optional<Car> car=this.carRepository.findById(id);
+        if (car.get().getState()==2){
+            throw new BusinessException(RentalMessages.carIsCurrentlyRented);
+        }
+    }
+    public void currentlyUnderMaintenance (int id){
+        Optional<Car> car=this.carRepository.findById(id);
+        if (car.get().getState()==3){
+            throw new BusinessException(RentalMessages.carIsCurrentlyUnderMaintance);
+        }
+    }
+}
